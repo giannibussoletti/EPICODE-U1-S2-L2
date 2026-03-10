686721c5bde0325b7922b0355a6ab2fd9bd1211b
Gianni <BussolettiGianni@gmail.com>
2026-03-10 09:39:30 +0100

    Update U1-S2-L2.js

--- scripts/U1-S2-L2.js
+++ scripts/U1-S2-L2.js
@@ -20,3 +20,29 @@ console.log("valore di variable1:", var1) // vedrei nella console del browser il
 console.log("quanto vale variable5:", var5) // vedrei nella console del browser il valore null
 // potete anche verificare il TIPO di una variabile
 console.log("andiamo a vedere il tipo di variable4:", typeof variable4)
+
+// le let sono riassegnabili
+variable1 = 101 // ammesso!
+
+// le const non sono riassegnabili
+const myName = "Stefano"
+// myName = 'Roberto' // non si può fare !
+console.log("ARRIVO QUI ??")
+
+// OPERATORI MATEMATICI
+// + - * / %
+let number1 = variable1 + 1 // 102
+number1 = number1 * 2 // 204
+console.log("number1 è 204?", number1)
+let divisione = number1 / 50
+console.log("divisione vale", divisione) // 4,08
+let resto = number1 % 20
+console.log("divisione vale", resto) // da 4 perché 20 nel 204 ci sta 10 volte e il resto è di 4.
+
+// OPERATORI COMPARAZIONE
+// delle operazioni logiche che restituiscono un valore booleano
+// > < >= <= === !==
+
+// AND (&&) e OR (||)
+// con AND si comparano 2 o più variabili e tutte devono essere vere
+// con OR anche si comparano 2 o più variabili ma va bene che anche solo 1 sia vera
