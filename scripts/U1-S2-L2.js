408bf0c47a4a8f9008d04109e078d69d6d9e99bf
Gianni <BussolettiGianni@gmail.com>
2026-03-10 09:09:56 +0100

    initial commit

---
+++ scripts/U1-S2-L2.js
@@ -0,0 +1,5 @@
+// RIASSUNTO DELLE PUNTATE PRECEDENTI
+
+// JS È UN LINGUAGGIO WEAKLY-TYPED ("debolmente tipizzato"): la creazione delle variabili
+// non dipende dal TIPO di DATO che dovete metterci dentro!
+// tutte le variabili si creano con le parole "let" e "const"
